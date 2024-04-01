2e4467d18027851361a0bccef30c2f8711a5742e
thiago alves <thiago87gsa@gmail.com>
2024-04-01 11:18:57 -0300

    fineshed 100%

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { Alegreya } from "next/font/google";
 import { IoMenu, IoClose } from "react-icons/io5";
 import { useState } from "react";
-
+import logo from "@/assets/img/logo.png";
+import Image from "next/image";
 
 const alegreya = Alegreya({ subsets: ["latin"] });
 
@@ -30,27 +31,31 @@ const Navbar = () => {
     <div className={`${alegreya.className} fixed w-full z-20`}>
       <nav className="flex justify-between items-center bg-colorGold">
         <div className="flex items-center justify-between w-3/5 mx-auto">
-        <Link
-          href="/"
-          className="hidden md:flex gap-2 items-center p-5 text-colorBlack hover:scale-105 cursor-pointer"
-        >
-          <p className="text-4xl font-semibold relative group overflow-hidden">
-            Logo
-          </p>
-        </Link>
-        <div className="hidden md:flex justify-end gap-8 p-5 text-colorBlack ">
-          {navigation.map((item) => (
-            <Link
-              className="text-sm uppercase font-semibold relative group overflow-hidden"
-              key={item.title}
-              href={item.href}
-            >
-              {item.title}
-              <span className="w-full h-[3px] bg-colorWhite absolute inline-block left-0 bottom-0 -translate-x-[101%] group-hover:translate-x-0 transition-transform duration-200" />
-            </Link>
-          ))}
-        </div>
-          
+          <Link
+            href="/"
+            className="hidden md:flex gap-2 items-center py-2 text-colorBlack hover:scale-105 cursor-pointer"
+          >
+            <p className="text-4xl font-semibold relative group overflow-hidden">
+              <Image
+                className="rounded-full"
+                alt="logo"
+                width={70}
+                src={logo}
+              />
+            </p>
+          </Link>
+          <div className="hidden md:flex justify-end gap-8 p-5 text-colorBlack ">
+            {navigation.map((item) => (
+              <Link
+                className="text-sm uppercase font-semibold relative group overflow-hidden"
+                key={item.title}
+                href={item.href}
+              >
+                {item.title}
+                <span className="w-full h-[3px] bg-colorWhite absolute inline-block left-0 bottom-0 -translate-x-[101%] group-hover:translate-x-0 transition-transform duration-200" />
+              </Link>
+            ))}
+          </div>
         </div>
       </nav>
       <div className="md:hidden flex justify-between p-4 items-center bg-colorGold">
@@ -62,7 +67,12 @@ const Navbar = () => {
                 className=" flex gap-2 items-center p-5 text-colorBlack hover:text-colorSecundary cursor-pointer"
               >
                 <p className="text-4xl font-semibold relative group overflow-hidden">
-                  Logo
+                  <Image
+                    className="rounded-full"
+                    alt="logo"
+                    width={70}
+                    src={logo}
+                  />
                 </p>
               </Link>
             </div>
@@ -74,14 +84,18 @@ const Navbar = () => {
         ) : (
           <div className="flex flex-col w-full justify-center ">
             <div className="flex justify-between items-center h-16">
-            <p className="text-4xl ml-5 text-colorBlack font-semibold relative group overflow-hidden">
-                Logo
+              <p className="text-4xl ml-5 text-colorBlack font-semibold relative group overflow-hidden">
+                <Image
+                  className="rounded-full"
+                  alt="logo"
+                  width={70}
+                  src={logo}
+                />
               </p>
               <IoClose
                 className="text-4xl cursor-pointer text-colorBlack"
                 onClick={toggleMenu}
               />
-              
             </div>
 
             <div className="bg-opacity-90 z-20 flex flex-col h-96 justify-around items-center rounded-lg">
